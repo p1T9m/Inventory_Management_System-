9c2541dbf460854c721bc22e1a451ec51e47dc27
p1T9m <tahamohammad2002@gmail.com>
2024-04-23 01:53:00 +0800

    project commit

---
+++ Sales.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class Sales {
+    
+    static SalesForm salesForm = new SalesForm();
+    static NewRegistration registerForm = new NewRegistration();
+    static UpdateForm updateForm = new UpdateForm();
+    static DeleteProducts deleteForm = new DeleteProducts();
+    
+}
